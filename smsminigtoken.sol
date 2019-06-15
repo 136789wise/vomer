3e36d0870ecfaff644de880cc3204880ecd97328
136789wise <51848223+136789wise@users.noreply.github.com>
2019-06-15 10:41:53 +0300

    Update smsminigtoken.sol

--- smsminigtoken.sol
+++ smsminigtoken.sol
@@ -5,11 +5,27 @@ pragma solidity ^0.4.13;
 */
 
 /*This contract was created for Vomer.
-*http://vomer.net/
+*https://vomer.net/
 *Vomer is not just an instant messenger.
 *Vomer is a lot. If not all.
 * 
-* 
+*This file is part of the Deviser Contract.
+
+*The Deviser Contract is free software: you can redistribute it and/or
+*modify it under the terms of the GNU lesser General Public License as published
+*by the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+
+*The Deviser Contract is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*GNU lesser General Public License for more details.
+
+*You should have received a copy of the GNU lesser General Public License
+*along with the Deviser Contract. If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*Designed by @136789wise
 */
 
 
